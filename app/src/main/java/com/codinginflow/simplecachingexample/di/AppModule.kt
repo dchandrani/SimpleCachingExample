e170ab3b6969ebc9c214e80da1402e11b7ad6757
Dhruv Chandrani <dhruv.chandrani@gmail.com>
2021-03-28 14:32:50 +0530

    Added room caching

--- app/src/main/java/com/codinginflow/simplecachingexample/di/AppModule.kt
+++ app/src/main/java/com/codinginflow/simplecachingexample/di/AppModule.kt
@@ -1,6 +1,9 @@
 package com.codinginflow.simplecachingexample.di
 
+import android.app.Application
+import androidx.room.Room
 import com.codinginflow.simplecachingexample.api.RestaurantApi
+import com.codinginflow.simplecachingexample.data.RestaurantDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -24,4 +27,12 @@ object AppModule {
     @Singleton
     fun provideRestaurantApi(retrofit: Retrofit): RestaurantApi =
         retrofit.create(RestaurantApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideDatabase(app: Application): RestaurantDatabase =
+        Room.databaseBuilder(app, RestaurantDatabase::class.java, "restaurant_database")
+            .build()
+
+
 }
--- app/src/main/java/com/codinginflow/simplecachingexample/features/restaurants/RestaurantViewModel.kt
+++ app/src/main/java/com/codinginflow/simplecachingexample/features/restaurants/RestaurantViewModel.kt
@@ -1,26 +1,26 @@
 package com.codinginflow.simplecachingexample.features.restaurants
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.codinginflow.simplecachingexample.api.RestaurantApi
 import com.codinginflow.simplecachingexample.data.Restaurant
+import com.codinginflow.simplecachingexample.data.RestaurantRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class RestaurantViewModel @Inject constructor(api: RestaurantApi) : ViewModel() {
-    private val restaurantsLiveData = MutableLiveData<List<Restaurant>>()
-    val restaurants: LiveData<List<Restaurant>> = restaurantsLiveData
+class RestaurantViewModel @Inject constructor(repository: RestaurantRepository) : ViewModel() {
+    val restaurants = repository.getRestaurants().asLiveData()
 
-    init {
-        viewModelScope.launch {
-            val restaurants = api.getRestaurants()
-            delay(2000)
-            restaurantsLiveData.value = restaurants
-        }
-    }
+//    private val restaurantsLiveData = MutableLiveData<List<Restaurant>>()
+//    val restaurants: LiveData<List<Restaurant>> = restaurantsLiveData
+//
+//    init {
+//        viewModelScope.launch {
+//            val restaurants = api.getRestaurants()
+//            delay(2000)
+//            restaurantsLiveData.value = restaurants
+//        }
+//    }
 }
